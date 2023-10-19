2380008e6e48b4a72542c22e15f57f746e139ce8
mateusribeirocampos <mateusrcampos@gmail.com>
2023-10-19 17:12:46 -0300

    small changes

--- Aula1.py
+++ Aula1.py
@@ -5,7 +5,6 @@ n2 = float(input("Informe a segunda nota: "))
 nome = str(input("Informe seu nome: "))
 m = (n1 + n2)/2
 if (m>= 60):
-    print("Aluno:", nome, "Aprovado! \nSua média foi de", m, "Nota1:", n1, "Nota2:", n2)
+    print("Aluno:",nome,"Aprovado! \nSua média foi de", m, "Nota1:", n1, "Nota2:", n2)
 else:
-    print("Aluno:", nome, "Reprovado! \nSua média foi de", m, "Nota1:", n1, "Nota2:", n2)
-    
+    print("Aluno:",nome,"Reprovado! \nSua média foi de", m, "Nota1:", n1, "Nota2:", n2)
