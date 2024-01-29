3189e9046eb370d764a3d9f8d36ad0f5ac394e35
mateusribeirocampos <mateusrcampos@gmail.com>
2024-01-29 17:11:27 -0300

    casas decimais

--- Aula2.py
+++ Aula2.py
@@ -15,3 +15,5 @@ nr = 2.99 / 4.1
 print("2.99/4.1=", nr)
 nr = (10.5-7.8)*(3.2+200.43)
 print("(10.5-7.8)*(3.2+200.43) =", nr)
+# imprimir o resultado com duas casas decimais
+print("Resultado com duas casas decimais: %.2f" % nr)
